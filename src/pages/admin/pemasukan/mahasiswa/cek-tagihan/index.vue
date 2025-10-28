89562681c181b2a6a72d9214a059890d1fbebe95
mfahmialif <mfahmialif@gmail.com>
2025-10-28 11:19:59 +0700

    update cek tagihan

--- src/pages/admin/pemasukan/mahasiswa/cek-tagihan/index.vue
+++ src/pages/admin/pemasukan/mahasiswa/cek-tagihan/index.vue
@@ -4,20 +4,84 @@ const listTagihan = ref([]);
 const total = ref("");
 const mahasiswa = reactive({});
 
+const mahasiswaList = ref([]);
+
+const search = ref("");
+const searchNim = ref("");
+const selectedMahasiswa = ref("");
+const loadingDataMahasiswa = ref(false);
+const loadingSearch = ref(false);
+
 const searchTagihan = () => {
   fetchTagihan();
+  fetchDeposit();
 };
 
+let typingTimeout = null;
+
+watch(search, (newVal) => {
+  clearTimeout(typingTimeout);
+
+  if (!newVal.trim()) {
+    mahasiswaList.value = [];
+    loadingSearch.value = false;
+    return;
+  }
+
+  typingTimeout = setTimeout(async () => {
+    try {
+      loadingSearch.value = true;
+      const res = await $api(`/admin/mahasiswa/search/${newVal}`, {
+        method: "GET",
+      });
+      // ubah hasil API jadi format { nim, nama, display: "nama - nim" }
+      mahasiswaList.value = res.map((m) => ({
+        ...m,
+        display: `${m.nim} - ${m.nama}`,
+      }));
+    } catch (err) {
+      showSnackbar({
+        text: err.data.message,
+        color: "error",
+      });
+      mahasiswaList.value = [];
+    } finally {
+      loadingSearch.value = false;
+    }
+  }, 1000); // <-- debounce 2 detik
+});
+
+watch(selectedMahasiswa, (newVal) => {
+  if (newVal && typeof newVal === "object" && !Array.isArray(newVal)) {
+    searchNim.value = newVal.nim;
+    searchTagihan();
+  } else if (typeof newVal === "string") {
+    searchNim.value = newVal;
+  } else if (!newVal) {
+    searchNim.value = "";
+  }
+});
+
 const fetchTagihan = async () => {
+  if (!searchNim.value) {
+    showSnackbar({
+      text: "NIM harus diisi",
+      color: "error",
+    });
+    return;
+  }
   try {
+    loadingDataMahasiswa.value = true;
     const response = await $api("/admin/pemasukan/mahasiswa/cek-tagihan", {
       method: "GET",
       body: {
-        nim: nim.value,
+        nim: searchNim.value,
       },
     });
 
-    mahasiswa.nim = nim.value;
+    console.log(response);
+
+    mahasiswa.nim = searchNim.value;
     mahasiswa.nama = response.data.nama_mhs;
     mahasiswa.angkatan = response.data.angkatan;
     mahasiswa.prodi = response.data.nama_prodi;
@@ -37,6 +101,33 @@ const fetchTagihan = async () => {
     }
   } catch (err) {
     console.error(err);
+    showSnackbar({
+      text: err.data.message,
+      color: "error",
+    });
+  } finally {
+    loadingDataMahasiswa.value = false;
+  }
+};
+
+const loadingDeposit = ref(false);
+const fetchDeposit = async () => {
+  try {
+    loadingDeposit.value = true;
+    const res = await $api(
+      `/admin/pemasukan/mahasiswa/catatan-deposit/nim/${searchNim.value}`,
+      {
+        method: "GET",
+      }
+    );
+    mahasiswa.deposit = res.jumlah;
+  } catch (error) {
+    showSnackbar({
+      text: error,
+      color: "error",
+    });
+  } finally {
+    loadingDeposit.value = false;
   }
 };
 
@@ -49,15 +140,25 @@ onMounted(() => {
   <div>
     <VRow class="mb-2">
       <VCol cols="12">
-        <VTextField
-          v-model="nim"
-          density="compact"
-          clearable
-          clear-icon="ri-close-line"
+        <VCombobox
+          v-model="selectedMahasiswa"
+          v-model:search="search"
+          :items="mahasiswaList"
+          item-title="display"
+          item-value="nim"
           label="NIM"
-          placeholder="Hey!!"
-          type="text"
+          clearable
+          :loading="loadingSearch"
         >
+          <template #append-inner>
+            <VProgressCircular
+              v-if="loadingSearch"
+              indeterminate
+              size="16"
+              width="2"
+            />
+          </template>
+
           <!-- Append -->
           <template #append>
             <VBtn
@@ -69,57 +170,65 @@ onMounted(() => {
               <span v-if="$vuetify.display.mdAndUp" class="ms-3">Search</span>
             </VBtn>
           </template>
-        </VTextField>
+        </VCombobox>
       </VCol>
     </VRow>
 
     <VCard class="mb-2">
-      <VCardItem class="pb-4">
-        <VCardTitle class="text-center">Tagihan Mahasiswa</VCardTitle>
-      </VCardItem>
+      <VSkeletonLoader
+        v-if="loadingDataMahasiswa"
+        type="article, list-item, paragraph, paragraph"
+      />
+      <template v-else>
+        <VCardItem class="pb-4">
+          <VCardTitle class="text-center">Tagihan Mahasiswa</VCardTitle>
+        </VCardItem>
 
-      <VDivider />
+        <VDivider />
 
-      <VTable class="table-detail">
-        <tbody>
-          <tr>
-            <td>NIM</td>
-            <td>:</td>
-            <td>{{ mahasiswa.nim }}</td>
-          </tr>
-          <tr>
-            <td>Nama Mahasiswa</td>
-            <td>:</td>
-            <td>{{ mahasiswa.nama }}</td>
-          </tr>
-          <tr>
-            <td>Program Studi</td>
-            <td>:</td>
-            <td>{{ mahasiswa.prodi }}</td>
-          </tr>
-          <tr>
-            <td>Tahun Angkatan</td>
-            <td>:</td>
-            <td>{{ mahasiswa.angkatan }}</td>
-          </tr>
-          <tr>
-            <td>Kelas</td>
-            <td>:</td>
-            <td>{{ mahasiswa.kelas }}</td>
-          </tr>
-          <tr>
-            <td>Deposit</td>
-            <td>:</td>
-            <td>asd</td>
-          </tr>
-        </tbody>
-      </VTable>
+        <VTable class="table-detail">
+          <tbody>
+            <tr>
+              <td>NIM</td>
+              <td>:</td>
+              <td>{{ mahasiswa.nim }}</td>
+            </tr>
+            <tr>
+              <td>Nama Mahasiswa</td>
+              <td>:</td>
+              <td>{{ mahasiswa.nama }}</td>
+            </tr>
+            <tr>
+              <td>Program Studi</td>
+              <td>:</td>
+              <td>{{ mahasiswa.prodi }}</td>
+            </tr>
+            <tr>
+              <td>Tahun Angkatan</td>
+              <td>:</td>
+              <td>{{ mahasiswa.angkatan }}</td>
+            </tr>
+            <tr>
+              <td>Kelas</td>
+              <td>:</td>
+              <td>{{ mahasiswa.kelas }}</td>
+            </tr>
+            <VSkeletonLoader type="list-item" v-if="loadingDeposit" />
+            <tr v-else>
+              <td>Deposit</td>
+              <td>:</td>
+              <td>{{ formatRupiah(mahasiswa.deposit) }}</td>
+            </tr>
+          </tbody>
+        </VTable>
+      </template>
 
       <VCardText class="d-flex flex-wrap gap-4"> </VCardText>
     </VCard>
 
     <VCard class="mb-2">
-      <VTable>
+      <VSkeletonLoader type="table" v-if="loadingDataMahasiswa"/>
+      <VTable v-else>
         <thead>
           <tr>
             <th class="text-uppercase">Tagihan</th>
@@ -135,10 +244,10 @@ onMounted(() => {
               {{ item.nama }}
             </td>
             <td>
-              {{ item.sisa }}
+              {{ formatRupiah(item.sisa) }}
             </td>
             <td>
-              {{ item.jumlah }}
+              {{ formatRupiah(item.jumlah) }}
             </td>
             <td>
               <span v-if="item.sisa <= 0">
@@ -150,8 +259,8 @@ onMounted(() => {
             </td>
           </tr>
           <tr>
-            <td>Total</td>
-            <td>{{ total }}</td>
+            <td><b>Total</b></td>
+            <td><b>{{ formatRupiah(total) }}</b></td>
           </tr>
         </tbody>
       </VTable>
